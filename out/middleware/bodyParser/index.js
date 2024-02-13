719cd744146a7466e4168ec31b6caaa1632cc277
Archery Luna <76516425+ArcheryLuna@users.noreply.github.com>
2024-02-13 19:30:44 +0000

    Removed Out directory

--- out/middleware/bodyParser/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function bodyParser(app) {
-    app.use(require('body-parser').json());
-}
-exports.default = bodyParser;
